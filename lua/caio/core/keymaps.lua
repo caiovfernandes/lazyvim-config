7cec7ebe25240139daca031a00aed94d9ac4c3bd
Caio Fernandes <caiovf13@gmail.com>
2024-01-25 16:36:55 -0300

    Add keymaps to save and quit

--- lua/caio/core/keymaps.lua
+++ lua/caio/core/keymaps.lua
@@ -23,13 +23,16 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decremen
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window -- 
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window --
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 
-
 --Terminal
-keymap.set("n", "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<CR>", {desc = "Horizontal Terminal" }) -- Horizontal Terminal,
+keymap.set("n", "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<CR>", { desc = "Horizontal Terminal" }) -- Horizontal Terminal,
+
+-- Save and Quit
+keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save File" }) -- Save File
+keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Exit File" }) -- Exit File
