d7faaaf0d8de5f99176da28a18dd615abebf59b9
Caio Fernandes <caiovf13@gmail.com>
2024-05-24 20:19:30 -0300

    Update plugins

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -8,6 +8,16 @@
 -- * dikksable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
+  { "echasnovski/mini.pairs", enabled = false },
+  {
+    "nyoom-engineering/oxocarbon.nvim"
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "oxocarbon"
+    }
+  },
   {"sindrets/diffview.nvim"},
   {
     "rmagatti/auto-session",
@@ -31,56 +41,6 @@ return {
   {
     "github/copilot.vim",
   },
-  {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    opts = {
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        semantic_tokens = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
-  },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "catppuccin",
-  --   },
-  -- },
-
-  -- change trouble config
   {
     "folke/trouble.nvim",
     -- opts will be merged with the parent spec
