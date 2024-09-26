ffd65a3ca7c2ef6f368cc70779dae69aec28a277
Caio Fernandes <caiovf13@gmail.com>
2024-09-26 18:41:51 -0300

    Remote neotree and add mini-files

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,16 +1,3 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      event_handlers = {
-        {
-          event = "file_opened",
-          handler = function(file_path)
-            -- Automatically close Neo-tree after opening a file
-            require("neo-tree.command").execute({ action = "close" })
-          end,
-        },
-      },
-    },
-  },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
